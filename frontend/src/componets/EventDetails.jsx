fe6ff67a9dcaad9a2f97e1b15fa8fe0551b68b2a
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-12-12 02:07:23 +0300

    Added axios for api fetching

--- frontend/src/componets/EventDetails.jsx
+++ frontend/src/componets/EventDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import axios from "axios"
 
 const EventDetail = () => {
   const { eventId } = useParams(); // Get the 'eventId' parameter from the URL
--- frontend/src/componets/EventList.jsx
+++ frontend/src/componets/EventList.jsx
@@ -1,108 +1,46 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import EventCard from './EventCard';
 import Navbar from './Navbar';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const EventList = () => {
-    const dummyEventData = [
-      {
-        id: 1,
-        title: 'Sample Event 1',
-        description: 'This is a sample event description.',
-        date: '2023-11-10',
-        time: '18:00',
-        location: 'Sample Location 1'
-      },
-      {
-        id: 2,
-        title: 'Sample Event 2',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 3,
-        title: 'Sample Event 3',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 4,
-        title: 'Sample Event 4',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 5,
-        title: 'Sample Event 5',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 6,
-        title: 'Sample Event 6',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 7,
-        title: 'Sample Event 1',
-        description: 'This is a sample event description.',
-        date: '2023-11-10',
-        time: '18:00',
-        location: 'Sample Location 1'
-      },
-      {
-        id: 8,
-        title: 'Sample Event 2',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      {
-        id: 9,
-        title: 'Sample Event 3',
-        description: 'This is another sample event description.',
-        date: '2023-11-15',
-        time: '14:00',
-        location: 'Sample Location 2'
-      },
-      // Add more dummy events as needed
-    ];
-  
-    return (
-      <>
-        <Navbar/>
-        <div className="event-list-page md:items-center">
-          <h1 className="text-3xl font-bold p-3 ">Upcoming Events</h1>
-          <div className="flex flex-wrap -mx-4">
-            {dummyEventData.map(event => (
-              <div key={event.id} className="w-full md:w-1/2 lg:w-1/3 px-4 mb-4">
-                <EventCard
-                  eventId={event.id} 
-                  title={event.title}
-                  description={event.description}
-                  date={event.date}
-                  time={event.time}
-                  location={event.location}
-                />
-              </div>
-            ))}
-          </div>
+  const [eventsData, setEventsData] = useState([]);
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const response = await axios.get('http://partypulse-q43v.onrender.com/api/events');
+        setEventsData(response.data); // Set the fetched event data to the state
+      } catch (error) {
+        console.error('Error fetching event data:', error);
+      }
+    };
+
+    fetchEventData(); // Call the function to fetch event data when the component mounts
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="event-list-page md:items-center">
+        <h1 className="text-3xl font-bold p-3">Upcoming Events</h1>
+        <div className="flex flex-wrap -mx-4">
+          {eventsData.map(event => (
+            <div key={event.id} className="w-full md:w-1/2 lg:w-1/3 px-4 mb-4">
+              <EventCard
+                eventId={event.id}
+                title={event.title}
+                description={event.description}
+                date={event.date}
+                time={event.time}
+                location={event.location}
+              />
+            </div>
+          ))}
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default EventList;
