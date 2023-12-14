4c92b31e78702702fb13dcb113201c9747d87b46
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-12-14 13:14:40 +0300

    Made some styling changes in the events details page

--- frontend/src/componets/EventDetails.jsx
+++ frontend/src/componets/EventDetails.jsx
@@ -41,23 +41,26 @@ const EventDetail = () => {
     <Navbar/>
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-400 to-pink-300 rounded-r-lg mt-2 mb-10">
       {/* Image on the left */}
-      <div className="mr-8">
+      <div className="mr-24 h-1/2 w-1/2 ">
         <UnsplashImage />
       </div>
 
       {/* Event details on the right */}
-      <div>
-        <h1 className="text-3xl font-bold mb-4">{title}</h1>
-        <p className="text-gray-600 mb-4">Description: {description}</p>
-        <p className="text-gray-600 mb-4">Date: {date}</p>
-        <p className="text-gray-600 mb-4">Time: {time}</p>
-        <p className="text-gray-600 mb-4">Location: {location}</p>
-      </div>
+      <div className="flex flex-col">
+        <div>
+          <h1 className="text-3xl font-bold mb-4">{title}</h1>
+          <p className="text-gray-600 mb-4">Description: {description}</p>
+          <p className="text-gray-600 mb-4">Date: {date}</p>
+          <p className="text-gray-600 mb-4">Time: {time}</p>
+        </div>
 
-      <div>
-        <button className="bg-purple-500 hover:bg-gradient-to-r from-orange-400 to-pink-300 py-3 text-white rounded-md w-full transition duration-300 ease-in-out mt-10">
-          <Link to="/ticketing">Book Event</Link>
-        </button>
+        {/* Location and Book Event button container */}
+        <div className="flex flex-col mt-4">
+          <p className="text-gray-600 mb-4">Location: {location}</p>
+          <button className="bg-purple-500 hover:bg-gradient-to-r from-orange-400 to-pink-300 py-3 text-white rounded-md w-full transition duration-300 ease-in-out">
+            <Link to="/ticketing">Book Event</Link>
+          </button>
+        </div>
       </div>
     </div>
     <Footer/>
