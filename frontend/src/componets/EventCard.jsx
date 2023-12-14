e8b94aabb97308a3aea06075fe6b2a3b2410a6a9
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-12-14 16:36:19 +0300

    added some shadow to the event card styling

--- frontend/src/componets/EventCard.jsx
+++ frontend/src/componets/EventCard.jsx
@@ -7,7 +7,7 @@ function EventCard({ eventId, title, description, date, time, location }) {
     return (
         
         <div className="">
-            <div className='bg-fuchsia-400 rounded shadow p-2 mb-4 w-64 '>
+            <div className='bg-fuchsia-500 rounded  p-2 mb-4 w-64 shadow-xl shadow-sky-300 '>
                 <Link to={`/events/${eventId}`} className="block">
                     <h2 className='text-xl text-slate-950 font-semibold mb-2'>{title}</h2>
                     <p className='text-gray-700 mb-2'>{description}</p>
